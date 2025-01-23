b1e3e1c3433f57a9d8a219c0c777c0ddb6e6671f
wmohamed24 <waelm.c24@gmail.com>
2025-01-23 16:45:03 -0500

    update counting code

--- QC/count_durations.py
+++ QC/count_durations.py
@@ -6,17 +6,19 @@ from tqdm import tqdm
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from mutagen.mp3 import MP3
 from mutagen.wave import WAVE
+from datetime import timedelta
 
 # Determine the language of the file based on the path
-def get_lang(path, langs):
+def get_lang(path, file, langs):
     for lang in langs:
-        if lang in path:
+        if lang in path or (file.split('.')[0] == lang and file.split('.')[1:] == ['xml']):
             return lang
 
-def process_file(file_path, langs):
+def process_file(path, file, langs):
     """Process a single file: determine language and duration."""
     try:
-        lang = get_lang(file_path, langs)
+        lang = get_lang(path, file, langs)
+        file_path = os.path.join(path, file)
         # audio = AudioSegment.from_file(file_path)
         # length_in_sec = len(audio) // 1000
         if file_path.endswith('.mp3'):
@@ -29,17 +31,16 @@ def process_file(file_path, langs):
     except Exception as e:
         return (file_path, None, None, e)
 
-def count_source(corpus, path, durations_by_lang, langs):
+def count_source_diarized(corpus, path, durations_by_lang, langs):
     source_total = 0
     to_process = list()
     for root, dirs, files in os.walk(path):
         for file in files:
             if (file.endswith(".wav") or file.endswith('.mp3')) and 'audio' in os.path.join(root, file):
-                file_path = os.path.join(root, file)
-                to_process.append(file_path)
+                to_process.append([root, file])
     
     with ThreadPoolExecutor() as executor:
-        futures = [executor.submit(process_file, file_path, langs) for file_path in to_process]
+        futures = [executor.submit(process_file, path, file, langs) for path, file in to_process]
 
         for f in tqdm(as_completed(futures), total=len(to_process), desc=f"processing audio for {corpus}"):
             file_path, lang, length, err = f.result()
@@ -51,9 +52,38 @@ def count_source(corpus, path, durations_by_lang, langs):
                 
     return source_total
     
+
+def read_file(file_path):
+
+    duration = 0
+    tree = ET.parse(file_path)
+    root = tree.getroot()
+    
+    # Iterate over all <S> elements
+    for audio in root.findall('.//AUDIO'):
+        # Find the <FORM> element within the <S> element
+        if 'start' not in audio.attrib or 'end' not in audio.attrib:
+            raise ValueError("start and end must be set to all audio tags if the audio isn't diarized")
+        # print(audio.attrib['end'])
+        # print(audio.attrib['start'])
+        duration += (float(audio.attrib['end']) - float(audio.attrib['start']))
+
+    return duration
+
+def count_source_nondiarized(path, durations_by_lang, langs):
+    source_total = 0
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith(".xml") and 'XML' in os.path.join(root, file):
+                lang = get_lang(root, file, langs)
+                durations_in_file = read_file(os.path.join(root, file))
+                durations_by_lang[lang] += durations_in_file
+                source_total += durations_in_file
+    durations_by_lang[lang] = round(durations_by_lang[lang], 2)
+    return round(source_total, 2)
     
 
-def get_durations(corpora_path):
+def get_durations(corpora_path, diarized):
     
     langs = ['Amis', 'Atayal', 'Paiwan', 'Bunun','Puyuma', 'Rukai', 'Tsou', 'Saisiyat', 'Yami',
         'Thao', 'Kavalan', 'Truku', 'Sakizaya','Seediq','Saaroa', 'Kanakanavu', 'Siraya']
@@ -67,13 +97,25 @@ def get_durations(corpora_path):
             continue
         durations_by_source[source] = 0
         print(f"\n=====counting in {source}======")
-        durations_by_source[source] = count_source(source, os.path.join(corpora_path, source), durations_by_lang, langs)
-
+        if diarized:
+            durations_by_source[source] = count_source_diarized(source, os.path.join(corpora_path, source), durations_by_lang, langs)
+        else:
+            durations_by_source[source] = count_source_nondiarized(os.path.join(corpora_path, source), durations_by_lang, langs)
+    
     return durations_by_lang, durations_by_source
-def main(corpora_path):
 
-    durations_by_lang, durations_by_source = get_durations(corpora_path)
-    total = sum(durations_by_lang.values())
+def convert_seconds_to_hms(seconds):
+    td = timedelta(seconds=seconds)
+    total_seconds = int(td.total_seconds())
+    hours, remainder = divmod(total_seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    return f"{hours:02}:{minutes:02}:{seconds:02}"
+    
+def main(corpora_path, non_diarized):
+    diarized = not non_diarized
+    durations_by_lang, durations_by_source = get_durations(corpora_path, diarized)
+   
+    total = round(sum(durations_by_lang.values()), 2)
 
     print("\n=====durations per language======")
     print(durations_by_lang)
@@ -82,18 +124,24 @@ def main(corpora_path):
     print("\n=====durations total-count======")
     print(total)
 
-    # total_seconds = 
+    new_durations_by_lang, new_durations_by_source = dict(), dict()
+    for item in durations_by_lang:
+        new_durations_by_lang[item] = convert_seconds_to_hms(durations_by_lang[item])
+    for item in durations_by_source:
+        new_durations_by_source[item] = convert_seconds_to_hms(durations_by_source[item])
+    new_total = convert_seconds_to_hms(total)
 
-    # hours = total_seconds // 3600
-    # minutes = (total_seconds % 3600) // 60
-    # seconds = total_seconds % 60
+    print("\n=====durations per language======")
+    print(new_durations_by_lang)
+    print("\n=====durations per source======")
+    print(new_durations_by_source)
+    print("\n=====durations total-count======")
+    print(new_total)
 
-    #print(f"Total audio duration: {hours}h {minutes}m {seconds}s")
     
-    # with open('current_counts.txt', 'w') as file:
-    #     json.dump(token_count, file)
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Get durations of audio per corpus and per language.")
     parser.add_argument('corpora_path', help='Specify the path of the corpora')
+    parser.add_argument('--non_diarized', action='store_true', help='use if the corpora isn not diarized')
     args = parser.parse_args()
-    main(args.corpora_path)
+    main(args.corpora_path, args.non_diarized)
--- QC/count_tokens.py
+++ QC/count_tokens.py
@@ -1,12 +1,11 @@
 import xml.etree.ElementTree as ET
 import os
 import argparse
-
+from collections import defaultdict
 # Determine the language of the file based on the path
-def get_lang(path, langs):
-    print(path)
+def get_lang(path, file, langs):
     for lang in langs:
-        if lang in path:
+        if lang in path or (file.split('.')[0] == lang and file.split('.')[1:] == ['xml']):
             return lang
 
 
@@ -15,7 +14,10 @@ def read_file(file_path):
     num_words = 0
     tree = ET.parse(file_path)
     root = tree.getroot()
-    
+    if "dialect" in root.attrib:
+        dialect = root.attrib['dialect']
+    else:
+        dialect = None
     # Iterate over all <S> elements
     for s in root.findall('.//S'):
         # Find the <FORM> element within the <S> element
@@ -27,16 +29,21 @@ def read_file(file_path):
             # Count the number of words
             num_words += len(words)
 
-    return num_words
+    return num_words, dialect
 
 def count_source(path, tokens_by_lang, langs):
     source_total = 0
     for root, dirs, files in os.walk(path):
         for file in files:
             if file.endswith(".xml") and 'XML' in os.path.join(root, file):
-                lang = get_lang(os.path.join(root, file), langs)
-                tokens_in_file = read_file(os.path.join(root, file))
-                tokens_by_lang[lang] += tokens_in_file
+                # print(root, file)
+                lang = get_lang(root, file, langs)
+                tokens_in_file, dialect = read_file(os.path.join(root, file))
+                if dialect:
+                    tokens_by_lang[lang][1][dialect] += tokens_in_file
+                else:
+                    tokens_by_lang[lang][1]['Not Specified'] += tokens_in_file
+                tokens_by_lang[lang][0] += tokens_in_file
                 source_total += tokens_in_file
     return source_total
     
@@ -47,7 +54,7 @@ def get_counts(corpora_path):
     langs = ['Amis', 'Atayal', 'Paiwan', 'Bunun','Puyuma', 'Rukai', 'Tsou', 'Saisiyat', 'Yami',
         'Thao', 'Kavalan', 'Truku', 'Sakizaya','Seediq','Saaroa', 'Kanakanavu', 'Siraya']
 
-    tokens_by_lang = {lang: 0 for lang in langs}
+    tokens_by_lang = {lang: [0, defaultdict(int)] for lang in langs}
     tokens_by_source = dict()
     for source in os.listdir(corpora_path):
         if source.startswith('.'):
@@ -62,13 +69,14 @@ def get_counts(corpora_path):
 def main(corpora_path):
 
     tokens_by_lang, tokens_by_source = get_counts(corpora_path)
-
+    for lang in tokens_by_lang:
+        print(lang, ": ", tokens_by_lang[lang], "\n\n")
     print("\n=====tokens count per language======")
     print(tokens_by_lang)
     print("\n=====tokens count per source======")
     print(tokens_by_source)
     print("\n=====tokens total count======")
-    print(sum(tokens_by_source.values()), sum(tokens_by_lang.values()))
+    print(sum(tokens_by_source.values()))
 
 
     # with open('current_counts.txt', 'w') as file:
