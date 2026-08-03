3d5f7efb9cf0bace346fdcb783cd394e830b517f
Hunter S <67876717+hunterschep@users.noreply.github.com>
2026-08-02 18:02:09 -0700

    Publish reproducible Glosbe restoration workflow

---
+++ Corpora/Glosbe/CodeAndDocs/scripts/dedupe_examples.py
@@ -0,0 +1,6 @@
+from glosbe_pipeline import main
+
+if __name__ == "__main__":
+    import sys
+    sys.argv.insert(1, "dedupe_examples")
+    main()
---
+++ Corpora/Glosbe/CodeAndDocs/scripts/discover_token_behavior.py
@@ -0,0 +1,6 @@
+from glosbe_pipeline import main
+
+if __name__ == "__main__":
+    import sys
+    sys.argv.insert(1, "discover_token_behavior")
+    main()
